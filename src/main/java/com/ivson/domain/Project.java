6e4c5d3bb889436e674a6272cae8fda2b4fc6bc2
ivsonsantos <Belgique@2024>
2021-08-12 15:42:48 +0200

    Json Date Format

--- src/main/java/com/ivson/domain/Project.java
+++ src/main/java/com/ivson/domain/Project.java
@@ -1,5 +1,6 @@
 package com.ivson.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -23,16 +24,23 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String projectName;
     private String projectIdentifier;
     private String description;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate start_date;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate end_date;
 
     @CreationTimestamp
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
     private LocalDateTime created_at;
 
     @UpdateTimestamp
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
     private LocalDateTime updated_at;
 
 }
